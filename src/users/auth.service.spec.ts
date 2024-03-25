3c3f0018bc43149828788c75992a21e601756967
Ahn Ming Loke <lokeahnming@gmail.com>
2024-03-25 16:12:50 -0400

    test: refactor test structure

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -3,29 +3,35 @@ import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
 import { UserEntity } from './users.entity';
 
-it('can create an instance of auth service', async () => {
-  // mock service
-  const dummyUserService: Partial<UsersService> = {
-    find: () => Promise.resolve([]),
-    create: (email: string, password: string) =>
-      Promise.resolve({
-        id: 1,
-        email,
-        password,
-      } as UserEntity),
-  };
+describe('AuthService', () => {
+  let service: AuthService;
 
-  const module = await Test.createTestingModule({
-    providers: [
-      AuthService,
-      {
-        provide: UsersService,
-        useValue: dummyUserService,
-      },
-    ],
-  }).compile();
+  beforeEach(async () => {
+    // mock service
+    const dummyUserService: Partial<UsersService> = {
+      find: () => Promise.resolve([]),
+      create: (email: string, password: string) =>
+        Promise.resolve({
+          id: 1,
+          email,
+          password,
+        } as UserEntity),
+    };
 
-  const service = module.get(AuthService);
+    const module = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: dummyUserService,
+        },
+      ],
+    }).compile();
 
-  expect(service).toBeDefined();
+    service = module.get(AuthService);
+  });
+
+  it('can create an instance of auth service', async () => {
+    expect(service).toBeDefined();
+  });
 });
