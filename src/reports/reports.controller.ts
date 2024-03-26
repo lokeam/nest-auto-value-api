9d7a30ca99569e7cbc73f564429bde039c298ca5
Ahn Ming Loke <lokeahnming@gmail.com>
2024-03-26 16:27:43 -0400

    feat: add report approval functionality to Controller + Reports Service

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,10 +1,18 @@
-import { Controller, Post, Body, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportsService } from './reports.service';
 import { AuthGuard } from 'src/guards/auth.guard';
 import { CurrentUser } from 'src/users/decorators/current-user.decorator';
 import { UserEntity } from 'src/users/users.entity';
 import { ReportDto } from './dtos/report.dto';
+import { ApproveReportDto } from './dtos/approve-report.dto';
 import { Serialize } from '../interceptors/serialize.interceptor';
 
 @Controller('reports')
@@ -16,5 +24,10 @@ export class ReportsController {
   @Serialize(ReportDto)
   createReport(@Body() body: CreateReportDto, @CurrentUser() user: UserEntity) {
     return this.reportsService.create(body, user);
+  }
+
+  @Patch('/:')
+  approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
+    return this.reportsService.changeApproval(id, body.approved);
   }
 }
