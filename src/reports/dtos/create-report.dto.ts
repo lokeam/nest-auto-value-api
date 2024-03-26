c40bfd6e4e243454bff65b27548e549f0eb3d9a1
Ahn Ming Loke <lokeahnming@gmail.com>
2024-03-26 14:22:43 -0400

    feat: add createReprot route on Reports Controller + CreateReportDto

---
+++ src/reports/dtos/create-report.dto.ts
@@ -0,0 +1,36 @@
+import {
+  IsString,
+  IsNumber,
+  Min,
+  Max,
+  IsLatitude,
+  IsLongitude,
+} from 'class-validator';
+
+export class CreateReportDto {
+  @IsString()
+  make: string;
+
+  @IsString()
+  model: string;
+
+  @Min(1930)
+  @Max(2080)
+  year: number;
+
+  @IsNumber()
+  @Min(0)
+  @Max(1000000)
+  mileage: number;
+
+  @IsLatitude()
+  lat: number;
+
+  @IsLongitude()
+  lng: number;
+
+  @IsNumber()
+  @Min(0)
+  @Max(1000000)
+  price: number;
+}
